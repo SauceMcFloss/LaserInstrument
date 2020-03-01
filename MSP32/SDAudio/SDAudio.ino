1ed015b5464f5c43ffee88fcf315f8cbb373f2d6
Luke Hazelton <42985814+SauceMcFloss@users.noreply.github.com>
2020-03-01 13:07:22 -0500

    All note sections code
    This code can play all sections of a single note at once, and show how to loop through the middle section.

--- MSP32/SDAudio/SDAudio.ino
+++ MSP32/SDAudio/SDAudio.ino
@@ -2,15 +2,17 @@
 #include <SD.h>
 #include <SPI.h>
 
-const long defaultWavLength = 54498;
-unsigned char A4Note[defaultWavLength];
+const long defaultWavLength = 17909;
+unsigned char A4Note_Beg[17174];
+unsigned char A4Note_Mid[17909];
+unsigned char A4Note_End[16623];
 //unsigned char BNote[defaultWavLength];
 //unsigned char CNote[defaultWavLength];
 //unsigned char DNote[defaultWavLength];
 //unsigned char ENote[defaultWavLength];
 //unsigned char FNote[defaultWavLength];
 unsigned char GNote[defaultWavLength];
-XT_DAC_Audio_Class DacAudio2(26,0);
+//XT_DAC_Audio_Class DacAudio2(26,0);
 XT_DAC_Audio_Class DacAudio1(25,0);
 
 void SetFileSize(unsigned char * list, long maximum){
@@ -32,6 +34,21 @@ void setup() {
   }
   long counter;
   File file;
+
+  Serial.println("---Stage 1 A---");
+  file = SD.open("/flute_A4_1_mezzo-forte_normal_beg.wav");
+  
+  Serial.println("---Stage 2 A---");
+  //SetFileSize(A4Note, defaultWavLength);
+  
+  Serial.println("---Stage 3 A---");
+  counter = 0;
+  while(file.available() && (counter < 17174)){
+    A4Note_Beg[counter] = (unsigned char)file.read();
+    counter++;
+  }
+  
+  file.close();
   
   Serial.println("---Stage 1 A---");
   file = SD.open("/flute_A4_1_mezzo-forte_normal_mid.wav");
@@ -41,22 +58,70 @@ void setup() {
   
   Serial.println("---Stage 3 A---");
   counter = 0;
-  while(file.available() && (counter < defaultWavLength)){
-    A4Note[counter] = (unsigned char)file.read();
+  while(file.available() && (counter < 17909)){
+    A4Note_Mid[counter] = (unsigned char)file.read();
     counter++;
   }
   
   file.close();
 
-  XT_Wav_Class PianoA4(A4Note);
+  Serial.println("---Stage 1 A---");
+  file = SD.open("/flute_A4_1_mezzo-forte_normal_end.wav");
   
-  while(true){
+  Serial.println("---Stage 2 A---");
+  //SetFileSize(A4Note, defaultWavLength);
+  
+  Serial.println("---Stage 3 A---");
+  counter = 0;
+  while(file.available() && (counter < 16623)){
+    A4Note_End[counter] = (unsigned char)file.read();
+    counter++;
+  }
+  
+  file.close();
+
+  XT_Wav_Class PianoA4_Beg(A4Note_Beg);
+  XT_Wav_Class PianoA4_Mid(A4Note_Mid);
+  XT_Wav_Class PianoA4_End(A4Note_End);
+
+  Serial.println(PianoA4_Beg.DataSize);
+  Serial.println(PianoA4_Mid.DataSize);
+  Serial.println(PianoA4_End.DataSize);
+
+  if(PianoA4_Beg.Playing==false){
+    DacAudio1.Play(&PianoA4_Beg);
+    PianoA4_Beg.Looped=false;
+    Serial.println("*beg*");
+  }
+  while(PianoA4_Beg.Looped == false){
     DacAudio1.FillBuffer();
-    if(PianoA4.Playing==false){
-      DacAudio1.Play(&PianoA4);
-      Serial.println("*note*");
+  }
+
+  for(int i = 0; i < 3; i++){
+    if(PianoA4_Mid.Playing==false){
+      DacAudio1.Play(&PianoA4_Mid);
+      PianoA4_Mid.Looped=false;
+      Serial.println("*mid*");
+    }
+    while(PianoA4_Mid.Looped == false){
+      DacAudio1.FillBuffer();
     }
   }
+
+  while(true){
+    if(PianoA4_End.Playing==false){
+      DacAudio1.Play(&PianoA4_End);
+      PianoA4_End.Looped=false;
+      Serial.println("*end*");
+    }
+    while(PianoA4_End.Looped == false){
+      DacAudio1.FillBuffer();
+    }
+  }
+  
+  DacAudio1.StopAllSounds();
+  Serial.println("Done.");
+  //}
 }
 
 void loop(){
