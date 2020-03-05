bfc9ea6d095b3825b4f19188eec0635f63377c99
Luke Hazelton <tomluke600@gmail.com>
2020-03-05 16:51:28 -0500

    Set Up for User Input
    Improved code so that the logic is simple, the functions are easy to use, and proved compatibility with transistors so all the improvements from this should be able to be easily copy/pasted into existing laser & lcd code.

--- esp32_sdAudio/esp32_sdAudio.ino
+++ esp32_sdAudio/esp32_sdAudio.ino
@@ -18,6 +18,10 @@ unsigned char* A4Note;
 unsigned char* B4Note;
 unsigned char* C5Note;
 
+int SensorThreshold = 150;
+
+int C4Sensor = A0; // anolog pin 0
+
 // Default functions
 
 // Default setup() function
@@ -39,10 +43,8 @@ void setup()
 
   // Test
 
-  PrepareC4Note();
-
-  PlayC4Note();
-
+  PrepareAllNotes_Flute();
+  
   // End program
 
   PowerDown();
@@ -50,7 +52,10 @@ void setup()
 
 // Default loop() function
 void loop(){
-  //DacAudio.FillBuffer(); // Always attempt to fill audio buffer, which plays any active sound item(s)
+  if(analogRead(C4Sensor) > SensorThreshold)
+  {
+    PlayNote(1, C4Sensor); // PlayNote (Note, Sensor) - Note = C4, C4Sensor = A0
+  }
 }
 
 // Other functions
@@ -76,32 +81,191 @@ int FindMallocSize (const char* filename)
   return ((temp[7] << 24 | temp[6] << 16 |temp[5] << 8 |temp[4]) + 8);
 }
 
-// Fill a playable note array with the associate .wav bytes
-void PrepareC4Note()
+// Inits all notes with playable flute data
+void PrepareAllNotes_Flute()
 {
-  free(C4Note);
-  long fileSize = FindMallocSize("/flute_C4_1_mezzo-forte_normal_mid.wav");
-  C4Note = (unsigned char*)malloc(fileSize * sizeof(unsigned char));
-  
-  File file = SD.open("/flute_C4_1_mezzo-forte_normal_mid.wav");
+  PrepareNote_Flute(1);
+  PrepareNote_Flute(2);
+  PrepareNote_Flute(3);
+  PrepareNote_Flute(4);
+  PrepareNote_Flute(5);
+  PrepareNote_Flute(6);
+  PrepareNote_Flute(7);
+  PrepareNote_Flute(8);
+}
+// Fill a playable note array at the selected index with the associate .wav bytes
+void PrepareNote_Flute(int noteSelector)
+{
+  File file;
+  char *filename;
   long counter = 0;
-  while(file.available() && (counter < fileSize)){
-    C4Note[counter] = (unsigned char)file.read();
-    counter++;
+
+  if(noteSelector == 1)
+  {
+    free(C4Note);
+    filename = "/flute_C4_1_mezzo-forte_normal_mid.wav";
+    C4Note = (unsigned char*)malloc(FindMallocSize(filename) * sizeof(unsigned char));
+    file = SD.open(filename);
+    while(file.available()){
+      C4Note[counter] = (unsigned char)file.read();
+      counter++;
+    }
   }
+  else if(noteSelector == 2)
+  {
+    free(D4Note);
+    filename = "/flute_D4_1_mezzo-forte_normal_mid.wav";
+    D4Note = (unsigned char*)malloc(FindMallocSize(filename) * sizeof(unsigned char));
+    file = SD.open(filename);
+    while(file.available()){
+      D4Note[counter] = (unsigned char)file.read();
+      counter++;
+    }
+  }
+  else if(noteSelector == 3)
+  {
+    free(E4Note);
+    filename = "/flute_E4_1_mezzo-forte_normal_mid.wav";
+    E4Note = (unsigned char*)malloc(FindMallocSize(filename) * sizeof(unsigned char));
+    file = SD.open(filename);
+    while(file.available()){
+      E4Note[counter] = (unsigned char)file.read();
+      counter++;
+    }
+  }
+  else if(noteSelector == 4)
+  {
+    free(F4Note);
+    filename = "/flute_F4_1_mezzo-forte_normal_mid.wav";
+    F4Note = (unsigned char*)malloc(FindMallocSize(filename) * sizeof(unsigned char));
+    file = SD.open(filename);
+    while(file.available()){
+      F4Note[counter] = (unsigned char)file.read();
+      counter++;
+    }
+  }
+  else if(noteSelector == 5)
+  {
+    free(G4Note);
+    filename = "/flute_G4_1_mezzo-forte_normal_mid.wav";
+    G4Note = (unsigned char*)malloc(FindMallocSize(filename) * sizeof(unsigned char));
+    file = SD.open(filename);
+    while(file.available()){
+      G4Note[counter] = (unsigned char)file.read();
+      counter++;
+    }
+  }
+  else if(noteSelector == 6)
+  {
+    free(A4Note);
+    filename = "/flute_A4_1_mezzo-forte_normal_mid.wav";
+    A4Note = (unsigned char*)malloc(FindMallocSize(filename) * sizeof(unsigned char));
+    file = SD.open(filename);
+    while(file.available()){
+      A4Note[counter] = (unsigned char)file.read();
+      counter++;
+    }
+  }
+  else if(noteSelector == 7)
+  {
+    free(B4Note);
+    filename = "/flute_B4_1_mezzo-forte_normal_mid.wav";
+    B4Note = (unsigned char*)malloc(FindMallocSize(filename) * sizeof(unsigned char));
+    file = SD.open(filename);
+    while(file.available()){
+      B4Note[counter] = (unsigned char)file.read();
+      counter++;
+    }
+  }
+  else if(noteSelector == 8)
+  {
+    free(C5Note);
+    filename = "/flute_C5_1_mezzo-forte_normal_mid.wav";
+    C5Note = (unsigned char*)malloc(FindMallocSize(filename) * sizeof(unsigned char));
+    file = SD.open(filename);
+    while(file.available()){
+      C5Note[counter] = (unsigned char)file.read();
+      counter++;
+    }
+  }
+  else
+  {
+    Serial.println("Error preparing note! Note selector value must be 1 to 8.");
+  }
+  
   file.close();
 }
 
-void PlayC4Note()
+// Play the note at the selected index while user input is detected at the specified sensor
+void PlayNote(int noteSelector, int sensor)
 {
-  XT_Wav_Class C4Note_Playable(C4Note);
-  C4Note_Playable.Speed = .82;
-  C4Note_Playable.RepeatForever = true;
-  DacAudio.Play(&C4Note_Playable);
+  if(noteSelector == 1)
+  {
+    XT_Wav_Class C4Note_Playable(C4Note);
+    C4Note_Playable.Speed = .82;
+    C4Note_Playable.RepeatForever = true;
+    DacAudio.Play(&C4Note_Playable);
+  }
+  else if(noteSelector == 2)
+  {
+    XT_Wav_Class D4Note_Playable(D4Note);
+    D4Note_Playable.Speed = .82;
+    D4Note_Playable.RepeatForever = true;
+    DacAudio.Play(&D4Note_Playable);
+  }
+  else if(noteSelector == 3)
+  {
+    XT_Wav_Class E4Note_Playable(E4Note);
+    E4Note_Playable.Speed = .82;
+    E4Note_Playable.RepeatForever = true;
+    DacAudio.Play(&E4Note_Playable);
+  }
+  else if(noteSelector == 4)
+  {
+    XT_Wav_Class F4Note_Playable(F4Note);
+    F4Note_Playable.Speed = .82;
+    F4Note_Playable.RepeatForever = true;
+    DacAudio.Play(&F4Note_Playable);
+  }
+  else if(noteSelector == 5)
+  {
+    XT_Wav_Class G4Note_Playable(G4Note);
+    G4Note_Playable.Speed = .82;
+    G4Note_Playable.RepeatForever = true;
+    DacAudio.Play(&G4Note_Playable);
+  }
+  else if(noteSelector == 6)
+  {
+    XT_Wav_Class A4Note_Playable(A4Note);
+    A4Note_Playable.Speed = .82;
+    A4Note_Playable.RepeatForever = true;
+    DacAudio.Play(&A4Note_Playable);
+  }
+  else if(noteSelector == 7)
+  {
+    XT_Wav_Class B4Note_Playable(B4Note);
+    B4Note_Playable.Speed = .82;
+    B4Note_Playable.RepeatForever = true;
+    DacAudio.Play(&B4Note_Playable);
+  }
+  else if(noteSelector == 8)
+  {
+    XT_Wav_Class C5Note_Playable(C5Note);
+    C5Note_Playable.Speed = .82;
+    C5Note_Playable.RepeatForever = true;
+    DacAudio.Play(&C5Note_Playable);
+  }
+  else
+  {
+    Serial.println("Error playing note! Note selector value must be 1 to 8.");
+  }
 
-  for (int i = 0; i < 20000; i++){ // This is where we put "while the user is pressing a key"
+  while(analogRead(sensor) > SensorThreshold) // Play the loaded audio until the specified sensor stops registering user input
+  {
     DacAudio.FillBuffer();
   }
+
+  DacAudio.StopAllSounds(); // End all audio, hopefully stopping all residual whines or buzzes from the speaker
 }
 
 // Called at the end of the program to safely shut down and free all memory
